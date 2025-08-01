cb1df3d824dbeae5cdaa6d481777f55dc74fb535
DevHannan-Official <dev.hannan.official@gmail.com>
2025-08-01 16:07:55 +0500

    Initialize the server

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
